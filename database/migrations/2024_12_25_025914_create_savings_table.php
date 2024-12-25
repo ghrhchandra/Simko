4bd0b1d531d43fe437ae412e47744383ca1d912b
ghrhchandra <kurniawan.gchandra@gmail.com>
2024-12-25 10:12:20 +0700

    first commit

---
+++ database/migrations/2024_12_25_025914_create_savings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('savings', function (Blueprint $table) {
+            $table->id('savings_id');
+            $table->id('member_id');
+            $table->string('savings_type');
+            $table->decimal('savings_id', 15, 2);
+            $table->date('deposite_date');
+            $table->decimal('balance', 15, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('savings');
+    }
+};
